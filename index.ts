503a007cda9325a407c576dcb322d26ac70f4373
saltyaom <saltyaom@gmail.com>
2023-11-15 23:36:46 +0700

    :tada: feat: init

--- index.ts
+++ index.ts
@@ -1,1 +1,38 @@
-console.log('Hello TypeScript')
+const api = 'https://api.frankfurter.app'
+
+interface CurrencyResult {
+    amount: number
+    base: string
+    date: string
+    rates: {
+        [a: string]: number
+    }
+}
+
+type Currency = 'USD' | 'JPY' | 'THB'
+
+const convertCurrent = ({
+    from,
+    to,
+    amount
+}: {
+    amount: number
+    from: Currency
+    to: Currency
+}) => {
+    return fetch(`${api}/latest?from=${from}&to=${to}&amount=${amount}`)
+        .then((x) => x.json() as any as CurrencyResult)
+        .then((a) => a)
+}
+
+const main = async () => {
+    const currency = await convertCurrent({
+        amount: 100,
+        from: 'THB',
+        to: 'USD'
+    })
+
+    console.log(currency)    
+}
+
+main()
